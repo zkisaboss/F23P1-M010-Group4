39cc25843c99a173d2b91eed33d48d157e0a3a5c
Zachary Kosove <51681731+zkisaboss@users.noreply.github.com>
2023-09-26 08:38:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import pandas as pd
 
-
-"""Group 4 Project"""
-
-
-"""Code: Opens a *.txt file and converts the text into a binary representation of that text."""
+"Group 4 Project"
 
 
-"""Decode: Opens a *.txt file with the binary codes and converts it to the characters that the binary represents. """
+"Code: Opens a *.txt file and converts the text into a binary representation of that text."
 
 
-"""Create python code to get the lists of binary numbers and characters from an Excel file.
-        These lists should be set up such that the index for the binary value
-        and its corresponding character/string value have the same list index.
-        Note: When you read in the new line character from Excel, it
-        becomes “\\n” instead of “\n” so you will need to fix it in your list.
-        • Your character list should contain all the characters mentioned in the
-        assignment as well as any strings that you also have in your Excel
-        table
-"""
+"Decode: Opens a *.txt file with the binary codes and converts it to the characters that the binary represents."
+
 
 wb = pd.read_excel("P1Dictionary.xyls", dtype=str)
 bins = list(wb["Bin"])
@@ -26,3 +15,15 @@ chars = list(wb["Char"])
 
 for i in range(len(bins)):
     print(bins[i], ": ", chars[i])
+
+# Open File
+text_output = open("TextOutput.txt")
+s1 = text_output.read()
+print(s1)
+text_output.close()
+
+bin_output = open("BinOutput.txt")
+s2 = bin_output.read()
+print(s2)
+bin_output.close()
+
