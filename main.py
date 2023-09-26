c4f86bd9977f481f7c550c22eae5894089ec60f0
Zachary Kosove <51681731+zkisaboss@users.noreply.github.com>
2023-09-26 08:45:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import pandas as pd
 "Group 4 Project"
 
 
-"Code: Opens a *.txt file and converts the text into a binary representation of that text."
+"Encode: Opens a *.txt file and converts the text into a binary representation of that text."
+
+
+def encoder(excel_file: str):
+    file = pd.read_excel(excel_file, dtype=str)
+    bins = list(file["Bin"])
+    chars = list(file["Char"])
+
+    for i in range(len(bins)):
+        print(bins[i], ": ", chars[i])
+
+    # Open File
+    text_output = open("TextOutput.txt")
+    s1 = text_output.read()
+    print(s1)
+    text_output.close()
+
+    bin_output = open("BinOutput.txt")
+    s2 = bin_output.read()
+    print(s2)
+    bin_output.close()
 
 
 "Decode: Opens a *.txt file with the binary codes and converts it to the characters that the binary represents."
 
 
-wb = pd.read_excel("P1Dictionary.xyls", dtype=str)
-bins = list(wb["Bin"])
-chars = list(wb["Char"])
-
-for i in range(len(bins)):
-    print(bins[i], ": ", chars[i])
-
-# Open File
-text_output = open("TextOutput.txt")
-s1 = text_output.read()
-print(s1)
-text_output.close()
-
-bin_output = open("BinOutput.txt")
-s2 = bin_output.read()
-print(s2)
-bin_output.close()
-
+def decoder():
+    pass
